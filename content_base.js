9f658f772a4ecd3d4a4c19c92fc0c9d16dd1a761
Kristin Fritsch <ich@kristin-fritsch.de>
2018-04-24 22:29:15 +0200

    WIP sidebar

--- content_base.js
+++ content_base.js
@@ -1,3 +1,37 @@
+function Sidebar(){
+  this.markup = '<div id="sidebar">' +
+      '<div class="sidebar_drawer sidebar_drawer--articles hidden">Wissenswert</div>' +
+      '<div class="sidebar_drawer sidebar_drawer--alternatives hidden">Empfehlungen</div>' +
+      '<div class="sidebar_drawer sidebar_drawer--diy hidden">Selbermachen</div>' +
+      '<div class="sidebar_drawer sidebar_drawer--used hidden">Gebraucht</div>' +
+      '<div class="sidebar_buttons">' +
+        '<div class="sidebar_button sidebar_button--articles" data-content="articles"></div>' +
+        '<div class="sidebar_button sidebar_button--alternatives" data-content="alternatives"></div>' +
+        '<div class="sidebar_button sidebar_button--diy" data-content="diy"></div>' +
+        '<div class="sidebar_button sidebar_button--used" data-content="used"></div>' +
+      '</div>' +
+    '</div>';
+}
+
+Sidebar.prototype = {
+ render: function() {
+   return this.markup;
+ },
+ setEventListener: function() {
+  $(".sidebar_button").click(this.onClickEvent);
+ },
+ onClickEvent: function(event) {
+  let requestedDrawer = $(this).data("content");
+  console.log("onClick " + requestedDrawer);
+  if ($(".sidebar_drawer--" + requestedDrawer).is(":visible")) {
+    $(".sidebar_drawer--" + requestedDrawer).hide("slow");
+  } else {
+    $(".sidebar_drawer").hide();
+    $(".sidebar_drawer--" + requestedDrawer).show("slow");
+  }
+ }
+}
+
 function EbayResult(title, link, imgSrc) {
   this.domain = "https://www.ebay-kleinanzeigen.de";
   this.title = title || "";
